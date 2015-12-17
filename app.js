6662a031dac84543e13a3e4726cb07168b5d2781
Rob Arlandson <rob_arlandson@optum.com>
2015-12-17 10:20:21 -0600

    updated conn string

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var device = require('azure-iot-device');
 console.log(device);
 // Define the connection string to connect to IoT Hub
-var connectionString = 'HostName=ird-iot.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=tuvky4hgnQrHPP2Thlxqm1bADykzQfPzXzvz3qEEKM4=';
+// var connectionString = 'HostName=ird-iot.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=tuvky4hgnQrHPP2Thlxqm1bADykzQfPzXzvz3qEEKM4=';
+
+var connectionString = 'HostName=HostName=ird-iot.azure-devices.net;DeviceId=myFirstDevice;SharedAccessKey=6aU52Ll34wVIYcpSgjBUWGMD6cDAYEMbpno9qWa+9FQ=';
 // Create the client instance specifying the preferred protocol
 
 // Set the connection string and device ID for the IoTHub connection
